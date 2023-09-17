e84bf5b2b0dfa272dddd0a4ee332bc4c47bf0f36
samuelalejo182004 <139184143+samuelalejo182004@users.noreply.github.com>
2023-09-17 12:56:44 -0500

    16

--- cajero.html.js
+++ cajero.html.js
@@ -7,8 +7,11 @@ const ingresar=document.getElementById('ingresar');
 const Cancel2=document.getElementById("cancel2");
 const modal4=document.getElementById("modal4");
 const retiro=document.getElementById('retiro');
-
+const password1=document.getElementById("username2");
 const cancel3=document.getElementById('cancel3');
+let togglePassword=document.getElementById('toggle');
+
+const password2=document.getElementById("username4");
 
 salir1.addEventListener('click',()=>{
     modal2.classList.add('show');
@@ -21,7 +24,9 @@ background.style.justifyContent="center";
 
 ingresar.addEventListener("click", ()=>{
     modal3.classList.add('show');
+
     background.style.display="flex";
+
     background.style.justifyContent="center";
     background.style.alignItems="center";
 
@@ -50,3 +55,27 @@ cancel3.addEventListener('click',()=>{
     background.style.display="none";
 
 } )
+
+function showHide(){
+    if(password1.type==='password'){
+        password1.setAttribute('type','text');
+        togglePassword.classList.add('hide');
+    } else{
+        password1.setAttribute('type','password');
+        togglePassword.classList.remove('hide')
+    }
+}
+
+function showHide2(){
+    if(password2.type==='password'){
+        password2.setAttribute('type','text');
+        togglePassword.classList.add('hide');
+    } else{
+        password2.setAttribute('type','password');
+        togglePassword.classList.remove('hide')
+    }
+}
+
+
+
+
